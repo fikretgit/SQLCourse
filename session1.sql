068d597f96d90c99044875931e9a6591c47eb5c2
fikretgit <silvermail1631@gmail.com>
2021-12-09 00:20:29 +0300

    first commit

---
+++ session1.sql
@@ -0,0 +1,13 @@
+--SELECT name FROM tracks;
+--SELECT Composer, name FROM tracks;
+--SELECT * FROM tracks;
+--SELECT DISTINCT Composer FROM tracks;
+--SELECT DISTINCT AlbumId, MediaTypeId from tracks;
+--SELECT name
+--FROM tracks 
+--WHERE Composer = "Jimi Hendrix"
+SELECT * 
+FROM invoices
+WHERE total > 10;
+
+SELECT * FROM invoices;
